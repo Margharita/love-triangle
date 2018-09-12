8e477513f671e3477bf656fc2dbe7d41b4b0674a
Margharita <ritus-margaritus@yandex.ru>
2018-09-12 14:19:31 +0300

    Created function for triangle counter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
+ * @param preferences = - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var counter = 0;
+      preferences.unshift(3);
+        for (i = 1;  i < preferences.length; i++){
+            if (i == preferences[preferences[preferences[i]]])
+            counter++;
+    }      counter = Math.floor(counter/3);
+    return counter;
+  
 };
